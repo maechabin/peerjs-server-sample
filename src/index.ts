97c1b7e34212731e919da4ba75cee6be41aa2b67
maeda <takanori@stroly.jp>
2020-04-06 02:54:43 +0900

    expressで実装

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
-import { PeerServer } from 'peer';
+import * as express from 'express';
+import { ExpressPeerServer } from 'peer';
 
-const peerServer = PeerServer({ port: 443, path: '/peer' });
+const app = express();
+
+app.get('/', (req, res, next) => res.send('Hello world!'));
+
+const http = require('http');
+
+const server = http.createServer(app);
+const peerServer = ExpressPeerServer(server, {
+  path: '/peer',
+}) as any;
+
+app.use('/peerjs', peerServer);
+
+server.listen(9000);
+
+// const peerServer = PeerServer({ port: 443, path: '/peer' });
